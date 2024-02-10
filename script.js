df379721cc53e696fca7d813b87deee9d2579f0f
Pratik Pawade <147968623+PratikPawade06@users.noreply.github.com>
2024-02-10 19:25:11 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+const parent = React.createElement('div', {id:'parent'}, React.createElement('div', {id:'child'},[ React.createElement('h1', {}, 'Hi Im h1 ') ,React.createElement('h2', {}, 'Hi Im h2 ')]), React.createElement('div', {id:'child'}, React.createElement('h1', {}, 'Hi Im h1 '), React.createElement('h2', {}, 'Hi Im h2 ') ) )
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent)
